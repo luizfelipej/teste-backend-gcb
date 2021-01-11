243f5891aa910b66b107317fe7714073d8410a3e
luizfelipej <luizfelipejesus94@gmail.com>
2021-01-11 19:47:40 -0300

    Projeto criado

---
+++ src/main/java/com/gcb/testbackend/resources/MedicoResource.java
@@ -0,0 +1,12 @@
+package com.gcb.testbackend.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/Medicos")
+public class MedicoResource {
+	
+	//implementar
+
+}

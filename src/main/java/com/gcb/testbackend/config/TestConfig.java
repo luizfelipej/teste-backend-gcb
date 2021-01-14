b5abdd2d92f2df73a8cb408ee82b3b9ff9b4e742
luizfelipej <luizfelipejesus94@gmail.com>
2021-01-14 11:03:59 -0300

    TestConfig commented

--- src/main/java/com/gcb/testbackend/config/TestConfig.java
+++ src/main/java/com/gcb/testbackend/config/TestConfig.java
@@ -26,19 +26,6 @@ public class TestConfig implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		
-		Doctor md1 = new Doctor(null, "Luiz", "439063218", "55847212", "941069682", "04313001");
-		Doctor md2 = new Doctor(null, "jorge", "439063219", "55847212", "941069682", "04313002");
-		doctorRepository.saveAll(Arrays.asList(md1, md2));
-		
-		
-		ExpertiseCategory esp1 = new ExpertiseCategory(null, DoctorExpertise.ALERGOLOGIA, md1);
-		ExpertiseCategory esp2 = new ExpertiseCategory(null, DoctorExpertise.ANGIOLOGIA, md1);
-		ExpertiseCategory esp3 = new ExpertiseCategory(null, DoctorExpertise.BUCO_MAXILO, md1);
-		
-		ExpertiseCategory esp4 = new ExpertiseCategory(null, DoctorExpertise.ALERGOLOGIA, md2);
-		ExpertiseCategory esp5 = new ExpertiseCategory(null, DoctorExpertise.CARDIOLOGIA_CLINICA, md2);
-		ExpertiseCategory esp6 = new ExpertiseCategory(null, DoctorExpertise.CIRURGIA_CABECA_E_PESCOCO, md2);
-		expertiseCategoryRepository.saveAll(Arrays.asList(esp1, esp2, esp3, esp4, esp5, esp6));
 		
 	}
 
